d28177b22a8a6d2bcb88245f94924165762de487
dmgcodevil <dmgcodevil@gmail.com>
2019-06-17 07:37:28 -0400

    allow CORS

--- site/server.js
+++ site/server.js
@@ -13,7 +13,11 @@ var express = require('express'),
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/parapet', { useNewUrlParser: true}); 
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
