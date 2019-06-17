b9ae917d55d42042358ec13e0e7697eab48575fd
Kirill Abramov <kirill.is.ninja.frontend@gmail.com>
2019-06-17 12:40:33 +0300

    relocate files

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,6 @@
-import Header from "./Header/Header";
-import UnderHeaderMenu from "./Menus/UnderHeaderMenu/UnderHeaderMenu";
-import DistributedSystem from "./DistributedSystem/DistributedSystem";
-import Services from "./Services/Services";
+import Header from "./MainPage/Header/Header";
+import UnderHeaderMenu from "./MainPage/Menus/UnderHeaderMenu/UnderHeaderMenu";
+import DistributedSystem from "./MainPage/DistributedSystem/DistributedSystem";
+import Services from "./MainPage/Services/Services";
 
 export { Header, UnderHeaderMenu, DistributedSystem, Services };
