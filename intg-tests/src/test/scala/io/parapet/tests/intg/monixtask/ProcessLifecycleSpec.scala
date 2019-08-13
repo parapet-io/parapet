e0dfd9758b7a1296259f91e5aa141f3a64c89f0f
dmgcodevil <dmgcodevil@gmail.com>
2019-08-13 04:10:40 -0400

    correct tests

--- intg-tests/src/test/scala/io/parapet/tests/intg/monixtask/ProcessLifecycleSpec.scala
+++ intg-tests/src/test/scala/io/parapet/tests/intg/monixtask/ProcessLifecycleSpec.scala
@@ -3,4 +3,4 @@ package io.parapet.tests.intg.monixtask
 import io.parapet.testutils.BasicMonixTaskSpec
 import monix.eval.Task
 
-class ProcessLifecycleSpec extends io.parapet.tests.intg.ChannelSpec[Task] with BasicMonixTaskSpec
+class ProcessLifecycleSpec extends io.parapet.tests.intg.ProcessLifecycleSpec[Task] with BasicMonixTaskSpec
--- intg-tests/src/test/scala/io/parapet/tests/intg/ziotask/ProcessLifecycleSpec.scala
+++ intg-tests/src/test/scala/io/parapet/tests/intg/ziotask/ProcessLifecycleSpec.scala
@@ -3,4 +3,4 @@ package io.parapet.tests.intg.ziotask
 import io.parapet.testutils.BasicZioTaskSpec
 import scalaz.zio.Task
 
-class ProcessLifecycleSpec extends io.parapet.tests.intg.ChannelSpec[Task] with BasicZioTaskSpec
+class ProcessLifecycleSpec extends io.parapet.tests.intg.ProcessLifecycleSpec[Task] with BasicZioTaskSpec
