643c873d57b06615a043d0e138c204312664afb0
dmgcodevil <dmgcodevil@gmail.com>
2021-02-10 02:27:04 -0500

    iss31: fix tests

--- core/src/test/scala/io/parapet/core/processes/RouletteLeaderElectionProtocolSpec.scala
+++ core/src/test/scala/io/parapet/core/processes/RouletteLeaderElectionProtocolSpec.scala
@@ -5,25 +5,35 @@ import io.parapet.core.processes.RouletteLeaderElection.{Ack, Announce, Heartbea
 import org.scalatest.FunSuite
 import org.scalatest.Matchers._
 
+import java.nio.ByteBuffer
+
 class RouletteLeaderElectionProtocolSpec extends FunSuite {
 
   test("propose") {
-    val data = RouletteLeaderElection.encoder.write(Propose("Propose", 0.85))
+    val data = addClientId(RouletteLeaderElection.encoder.write(Propose("Propose", 0.85)))
     RouletteLeaderElection.encoder.read(data) shouldBe Propose("Propose", 0.85)
   }
 
   test("ack") {
-    val data = RouletteLeaderElection.encoder.write(Ack("Ack", 0.86, AckCode.OK))
+    val data = addClientId(RouletteLeaderElection.encoder.write(Ack("Ack", 0.86, AckCode.OK)))
     RouletteLeaderElection.encoder.read(data) shouldBe Ack("Ack", 0.86, AckCode.OK)
   }
 
   test("announce") {
-    val data = RouletteLeaderElection.encoder.write(Announce("Announce"))
+    val data = addClientId(RouletteLeaderElection.encoder.write(Announce("Announce")))
     RouletteLeaderElection.encoder.read(data) shouldBe Announce("Announce")
   }
 
   test("heartbeat") {
-      val data = RouletteLeaderElection.encoder.write(Heartbeat("Heartbeat", Option.empty))
+    val data = addClientId(RouletteLeaderElection.encoder.write(Heartbeat("Heartbeat", Option.empty)))
     RouletteLeaderElection.encoder.read(data) shouldBe Heartbeat("Heartbeat", Option.empty)
+  }
+
+  private def addClientId(source: Array[Byte]): Array[Byte] = {
+    val buf = ByteBuffer.allocate(4 + source.length)
+    buf.putInt(0)
+    buf.put(source)
+    buf.rewind()
+    buf.array()
   }
 }
