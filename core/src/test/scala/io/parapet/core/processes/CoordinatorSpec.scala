c4257ffbda98bf565d018b9ca8fd2d72c40520b2
dmgcocodevil <dmgcdevil@gmail.com>
2021-11-13 20:06:55 -0500

    CoordinatorSpec

--- core/src/test/scala/io/parapet/core/processes/CoordinatorSpec.scala
+++ core/src/test/scala/io/parapet/core/processes/CoordinatorSpec.scala
@@ -32,6 +32,28 @@ class CoordinatorSpec extends AnyFunSuite {
       Message(Coordinator.Timeout, coordinatorRef))
   }
 
+  test("a node receives propose with higher number") {
+    // given
+    val coordinatorRef = ProcessRef("coordinator")
+    val p1 = ProcessRef("p1")
+    val p2 = ProcessRef("p2")
+    val execution = new Execution()
+    val coordinator = new Coordinator[Id](coordinatorRef, "p1", p1,
+      peers = Map("p2" -> p2), 0.5, generator(0.1))
+
+    // when
+    coordinator(Propose("p2", 0.86)).foldMap(IdInterpreter(execution))
+
+    // then
+    execution.print()
+    
+    execution.trace.exists {
+      case Message(Ack("p1", 0.86, AckCode.Ok), p2) => true
+      case _ => false
+    } shouldBe true
+
+  }
+
   private def generator(value: Double): Generator = {
     new Generator {
       override def generate: Coordinator.MinMax = Coordinator.MinMax(value, value)
