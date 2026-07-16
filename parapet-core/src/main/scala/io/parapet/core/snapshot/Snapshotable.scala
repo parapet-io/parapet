b6bcfed6735215b231f7a5421a5dc20953107eb6
dmgcodevil <dmgcodevil@gmail.com>
2026-07-15 23:38:34 -0400

    Snapshot

--- parapet-core/src/main/scala/io/parapet/core/snapshot/Snapshotable.scala
+++ parapet-core/src/main/scala/io/parapet/core/snapshot/Snapshotable.scala
@@ -1,6 +1,8 @@
 package io.parapet.core.snapshot
 
-/** A process whose state can be captured and restored. The process owns serialization.
+/** A process whose state can be captured and restored. The process owns serialization. A snapshotable process must have
+  * a stable reference. If the same process gets a new refernce (renamed), it will have a brand-new snapshot history of
+  * snapshots, that has no reference to previous snapshots created using old reference.
   */
 trait Snapshotable:
 
