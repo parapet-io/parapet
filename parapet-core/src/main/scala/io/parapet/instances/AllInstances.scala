86f0c647580a7319698b914fbfe2ff6efeadc2ac
dmgcodevil <dmgcodevil@gmail.com>
2026-07-28 08:51:40 -0400

    deleted AllInstances

--- parapet-core/src/main/scala/io/parapet/instances/AllInstances.scala
+++
@@ -1,8 +0,0 @@
-package io.parapet.instances
-
-/** Aggregator trait for typeclass instances exposed by parapet.
-  *
-  * Currently a placeholder - instances live in backend modules. Mix in via [[io.parapet.instances.all]] for forward
-  * compatibility.
-  */
-trait AllInstances
